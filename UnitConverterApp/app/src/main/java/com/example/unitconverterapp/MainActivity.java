0aa71b4e842b59598af04360689d86726facd86e
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-09-12 21:33:09 -0700

    Testing + fixing

--- UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.unitconverterapp;
 
 import android.os.Bundle;
-import android.widget.AdapterView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -48,8 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // listens for the button click and responds accordingly when one occurs
         //-----------------------------------------------------------------------------------------------
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 // get values of spinners
                 String fromUnit = spinFrom.getSelectedItem().toString();
                 String toUnit = spinTo.getSelectedItem().toString();
@@ -58,53 +55,29 @@ public class MainActivity extends AppCompatActivity {
                 String input = inputText.getText().toString();
 
                 // variables for the conversion
-                double inputTemp = Double.parseDouble(input);
+                double inputTemp;
 
-                if(fromUnit.equals(toUnit))
-                {
+                if (fromUnit.equals(toUnit)) {
                     // nothing happens
                     results.setText("");
                     noConversion.show();
-                }
-                else if(input.isEmpty())
-                {
-                    results.setText(getResources().getString(R.string.noInput));
-                    noValue.show();
-                }
-                else if(fromUnit.equals("Fahrenheit"))
-                {
-                    double celsius = (inputTemp - 32) * (float)(5/9);
-                    results.setText(String.format(Locale.CANADA, "%.2f", celsius));
-                }
-                else
-                {
-                    double fahr = (inputTemp * 9 / 5) + 32;
-                    results.setText(String.format(Locale.CANADA, "%.2f", fahr));
+                } else {
+                    try {
+                        inputTemp = Double.parseDouble(input);
+                        if (fromUnit.equals("Fahrenheit")) {
+                            double celsius = (inputTemp - 32) * (5.0 / 9.0);
+                            results.setText(String.format(Locale.CANADA, "%.2f", celsius));
+                        } else {
+                            double fahrenheit = (inputTemp * 9.0 / 5.0) + 32;
+                            results.setText(String.format(Locale.CANADA, "%.2f", fahrenheit));
+                        }
+                    } catch (NumberFormatException e)
+                    {
+                        results.setText(getResources().getString(R.string.noInput));
+                        noValue.show();
+                    }
                 }
             }
         });
-
-
-        // pseudocode
-
-        /*
-        if spinner 1 is fahrenheit, true (fahrenheit is true, celsius is false)
-        if spinner 2 is fahrenheit, true (fahrenheit is true, celsius is false)
-
-        on a button press:
-            check if spinner1 = spinner2
-                if yes, then nothing to convert
-                    update the result to equal the entered value (not converted)
-                    push the corresponding toast message
-            if the value box is empty
-                set the result to null
-                push the corresponding toast message
-            if spinner 1 is fahrenheit
-                convert from fahrenheit to celsius
-            convert from celsius to fahrenheit
-
-         */
     }
-
-
 }
